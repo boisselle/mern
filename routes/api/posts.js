0e05776b88150dc03c353b31a499c0867e036d1f
SEAN <augmentedsean@gmail.com>
2020-07-04 22:54:57 -0700

    added profiles, users, posts and authentication

---
+++ routes/api/posts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+// @route   GET api/posts
+// @desc    Test routes
+// @access  Public
+
+router.get('/', (req, res) => res.send('Posts route'));
+
+module.exports = router;
