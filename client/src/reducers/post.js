a1f4d74d8fd2c94b9f221ed08384fdfa34acfece
boisselle <smb@gatech.edu>
2020-10-12 23:52:56 -0700

    like and unlike functionality

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,6 +1,8 @@
 import {
     GET_POSTS, 
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKES,
+    UPDATE_PROFILE
 } from '../actions/types'; 
 
 const initialState = {
@@ -26,6 +28,12 @@ export default function(state = initialState, action) {
                 error: payload,
                 loading: false
             };
+        case UPDATE_LIKES:
+            return {
+                ...state,
+                posts: state.posts.map(post => post._id === payload.id ? { ...post, likes: payload.likes } : post),
+                loading: false 
+            }
         default:
             return state;
     }
