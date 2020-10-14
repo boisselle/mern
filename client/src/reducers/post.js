913de94b47172b741f06ac4a12bd0c8e6146908e
boisselle <smb@gatech.edu>
2020-10-13 23:37:04 -0700

    delete posts

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -2,7 +2,8 @@ import {
     GET_POSTS, 
     POST_ERROR,
     UPDATE_LIKES,
-    UPDATE_PROFILE
+    UPDATE_PROFILE,
+    DELETE_POST
 } from '../actions/types'; 
 
 const initialState = {
@@ -20,6 +21,12 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 posts: payload,
+                loading: false
+            };
+        case DELETE_POST:
+            return {
+                ...state,
+                posts: state.posts.filter(post._id !== payload),
                 loading: false
             };
         case POST_ERROR:
